8bcbc07122d2d435c5504bbb5829db1e345c069d
Mike Simmonds <msimmonds@zonedigital.com>
2018-07-28 17:08:38 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,68 @@
+import React, { Component } from "react";
+import "./app.css";
+import Segment from "./Segment";
+import cheerAudio from "./cheer.mp3";
+
+class App extends Component {
+  state = {
+    target: new Date(this.props.target).getTime()
+  };
+
+  componentDidMount = () => {
+    this.calculate();
+
+    this.interval = setInterval(this.calculate, 1000);
+  };
+
+  calculate = () => {
+    const now = Date.now();
+    const difference = this.state.target - now;
+    const done = difference < 1000;
+
+    const days = done ? 0 : Math.floor(difference / (1000 * 60 * 60 * 24));
+    const hours = done
+      ? 0
+      : Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = done
+      ? 0
+      : Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = done ? 0 : Math.floor((difference % (1000 * 60)) / 1000);
+
+    if (done) {
+      clearInterval(this.interval);
+    }
+
+    this.setState({
+      difference,
+      done,
+      days,
+      hours,
+      minutes,
+      seconds
+    });
+  };
+
+  render() {
+    const { done, days, hours, minutes, seconds } = this.state;
+    return (
+      <div className={`app${done ? " app--done" : ""}`}>
+        {done && <p className="app__done">Time to go!</p>}
+        {done && (
+          <audio autoPlay loop>
+            <source type="audio/mpeg" src={cheerAudio} />
+          </audio>
+        )}
+        {!done && (
+          <div className="app__segments">
+            <Segment number={days} unit="day" />
+            <Segment number={hours} unit="hour" />
+            <Segment number={minutes} unit="minute" />
+            <Segment number={seconds} unit="second" />
+          </div>
+        )}
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/Segment.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "./segment.css";
+
+const Segment = ({ number, unit }) => (
+  <div className="segment">
+    <span className="segment__number">{number}</span>{" "}
+    <span className="segment__unit">
+      {unit}
+      {number !== 1 && "s"}
+    </span>
+  </div>
+);
+
+export default Segment;
