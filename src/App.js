64ee2cede6d9d02d7b1bdd98caf3094e8385d531
Mike Simmonds <msimmonds@zonedigital.com>
2018-08-11 10:45:45 +0100

    Refresh
    - Adding editorconfig and prettier
    - Adding license
    - Reformatting
    - Cleaning up readme
    - Cleaning up package.json

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React, { Component } from "react";
-import "./app.css";
-import Segment from "./Segment";
-import cheerAudio from "./cheer.mp3";
+import React, { Component } from 'react'
+import './app.css'
+import Segment from './Segment'
+import cheerAudio from './cheer.mp3'
 
 class App extends Component {
   state = {
@@ -9,31 +9,31 @@ class App extends Component {
     days: 0,
     hours: 0,
     minutes: 0,
-    seconds: 0
-  };
+    seconds: 0,
+  }
 
   componentDidMount = () => {
-    this.calculate();
+    this.calculate()
 
-    this.interval = setInterval(this.calculate, 1000);
-  };
+    this.interval = setInterval(this.calculate, 1000)
+  }
 
   calculate = () => {
-    const now = Date.now();
-    const difference = this.state.target - now;
-    const done = difference < 1000;
+    const now = Date.now()
+    const difference = this.state.target - now
+    const done = difference < 1000
 
-    const days = done ? 0 : Math.floor(difference / (1000 * 60 * 60 * 24));
+    const days = done ? 0 : Math.floor(difference / (1000 * 60 * 60 * 24))
     const hours = done
       ? 0
-      : Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      : Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
     const minutes = done
       ? 0
-      : Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
-    const seconds = done ? 0 : Math.floor((difference % (1000 * 60)) / 1000);
+      : Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60))
+    const seconds = done ? 0 : Math.floor((difference % (1000 * 60)) / 1000)
 
     if (done) {
-      clearInterval(this.interval);
+      clearInterval(this.interval)
     }
 
     this.setState({
@@ -42,14 +42,14 @@ class App extends Component {
       days,
       hours,
       minutes,
-      seconds
-    });
-  };
+      seconds,
+    })
+  }
 
   render() {
-    const { done, days, hours, minutes, seconds } = this.state;
+    const { done, days, hours, minutes, seconds } = this.state
     return (
-      <div className={`app${done ? " app--done" : ""}`}>
+      <div className={`app${done ? ' app--done' : ''}`}>
         {done && <p className="app__done">Time to go!</p>}
         {done && (
           <audio autoPlay loop>
@@ -65,8 +65,8 @@ class App extends Component {
           </div>
         )}
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import registerServiceWorker from "./registerServiceWorker";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import registerServiceWorker from './registerServiceWorker'
 
 ReactDOM.render(
   <App target={new Date(2018, 7, 17)} />,
-  document.getElementById("root")
-);
-registerServiceWorker();
+  document.getElementById('root')
+)
+registerServiceWorker()
