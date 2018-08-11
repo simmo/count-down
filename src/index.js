c7ec66d275557d69081536dd590512b79e89be35
Mike Simmonds <msimmonds@zonedigital.com>
2018-08-11 10:49:34 +0100

    Adjusting date

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App'
 import registerServiceWorker from './registerServiceWorker'
 
 ReactDOM.render(
-  <App target={new Date(2018, 7, 17)} />,
+  <App target={new Date(2018, 7, 16, 18)} />,
   document.getElementById('root')
 )
 registerServiceWorker()
